fa27aad54c71675b2817f1a856f29dfc1579f155
nimod7890 <nimod1234@g.skku.edu>
2024-07-23 01:57:33 +0900

    chore: user repo

--- packages/admin/.yarn/.vite/deps_temp_f89003ab/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-4AVUGNG3.js";
-import "./chunk-XOTHJUM4.js";
-export default require_jsx_runtime();
-//# sourceMappingURL=react_jsx-runtime.js.map
--- packages/user/vite.config.ts
+++ packages/user/vite.config.ts
@@ -1,16 +1,26 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig, InlineConfig, UserConfig } from 'vite';
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 export default defineConfig({
 	plugins: [react()],
+	server: { port: 3000 },
 	css: {
 		postcss: path.resolve(__dirname, '../../postcss.config.js'),
 	},
 	cacheDir: './.yarn/.vite',
+	test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './vitest.setup.ts',
+  },
 	resolve: {
 		alias: {
 			src: path.resolve(__dirname, './src'),
 		},
 	},
-});
+} as VitestConfigExport);
