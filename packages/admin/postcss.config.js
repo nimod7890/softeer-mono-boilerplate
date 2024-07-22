08c7c62f99b4ad7ac9ec2524d6490bf4d35ae513
nimod7890 <nimod1234@g.skku.edu>
2024-07-23 01:35:38 +0900

    chore: shadcn

--- packages/admin/postcss.config.js
+++ packages/admin/postcss.config.js
@@ -1,8 +1,6 @@
-import path from 'path';
-
 export default {
 	plugins: {
-		tailwindcss: { config: path.resolve(__dirname, '../../postcss.config.js') },
+		tailwindcss: {},
 		autoprefixer: {},
 	},
 };
